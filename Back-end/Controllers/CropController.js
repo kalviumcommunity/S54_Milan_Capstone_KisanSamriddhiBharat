ac5f6fd89bff6410aa8ac861f0f06372b1d1ebae
Milan5002 <milan.sana@kalvium.community>
2024-03-21 13:00:55 +0530

    updated routes

--- Back-end/Controllers/CropController.js
+++ Back-end/Controllers/CropController.js
@@ -13,12 +13,21 @@ const getAllCrops = async (req, res) => {
 const getOneCropByID = async (req, res) => {
   try {
     const id = req.params.id;
-    res.status(200).json({message:`Crop(${id}) has been retrieved from the database.`})
+
+    if (!isValidObjectId(id)) {
+      return res.status(400).json({ message: "Invalid crop ID format." });
+    }
+
+
+    res.status(200).json({ message: `Crop(${id}) has been retrieved from the database.` });
   } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error in receiving data", error: error.message });
+    res.status(500).json({ message: "Error in receiving data", error: error.message });
   }
 };
+
+const isValidObjectId = (id) => {
+  return /^[0-9a-fA-F]{24}$/.test(id);
+};
+
 
 module.exports = { getAllCrops, getOneCropByID };
