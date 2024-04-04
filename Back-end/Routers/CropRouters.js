cce3d1c903213330e0a7c1d6e4758f601f86d1aa
Milan5002 <milan.sana@kalvium.community>
2024-04-04 16:14:05 +0530

    mongoDB conected and data added

--- Back-end/Routers/CropRouters.js
+++ Back-end/Routers/CropRouters.js
@@ -11,6 +11,6 @@ const CropRouter = express.Router();
 CropRouter.get("/all", getAllCrops);
 CropRouter.get("/one/:id", getOneCropByID);
 CropRouter.post("/post",postCrops)
-mealRouter.put("/put/:id", updateCropID);
+CropRouter.put("/put/:id", updateCropID);
 
 module.exports = CropRouter;
