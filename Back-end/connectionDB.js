b9200c082352bf3bd9da80f074c039c9bb9eac22
Milan5002 <milan.sana@kalvium.community>
2024-04-05 12:12:12 +0530

    mongoDB conected

--- Back-end/connectionDB.js
+++ Back-end/connectionDB.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
-console.log(process.env.MONGOURL); 
+console.log("MONGOURL from env:", process.env.MONGOURL); // Log MONGOURL to verify if it's correctly loaded
 const mongoose = require("mongoose");
 const mongoURI = process.env.MONGOURL;
+console.log("mongoURI:", mongoURI); // Log mongoURI to track its value
 const connectDB = async () => {
     try {
-        await mongoose.connect(mongoURI);
+        await mongoose.connect("mongodb+srv://milansana73:Manish321m@cluster0.ssq6uve.mongodb.net/Scheme");
         console.log("DataBase Connected");
 
     } catch (error) {
